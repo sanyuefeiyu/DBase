165463d8d4d2ff030f844f29116186cbed602285
sanyuefeiyu <shishuanghe@gmail.com>
2018-09-21 01:08:27 +0800

    add android test project

---
+++ test/android/app/src/main/cpp/DLog.h
@@ -0,0 +1,37 @@
+/*
+ * @author Double
+ * @since 2016/04/17
+ */
+
+#ifndef D_LOG_H
+#define D_LOG_H
+
+typedef enum
+{
+    DLOG_D = 0,
+    DLOG_I,
+    DLOG_W,
+    DLOG_E
+} DLogLevel;
+
+typedef enum
+{
+    DLOG_NONE = 0x0,
+    DLOG_CONSOLE = 0x1,
+    DLOG_FILE = 0x1 << 1,
+    DLOG_ALL = (DLOG_CONSOLE | DLOG_FILE )
+} DLogMode;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void DLog(DLogLevel level, const char *tag, const char *format, ...);
+void DLogSetOutputControl(DLogLevel logLevel, DLogMode logMode);
+void DLogSetOutputPath(const char *path);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* D_LOG_H */
