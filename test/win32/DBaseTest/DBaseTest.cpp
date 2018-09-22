13f8d70190fe97cf0ee03ab8d7d8322ac3ebb92d
sanyuefeiyu <shishuanghe@gmail.com>
2018-09-22 23:32:31 +0800

    add win32 test project

---
+++ test/win32/DBaseTest/DBaseTest.cpp
@@ -0,0 +1,16 @@
+// DBaseTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include "DLog.h"
+
+#define TAG "DBaseTest"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    DLogD(TAG, "%s", "hello world");
+
+	return 0;
+}
+
