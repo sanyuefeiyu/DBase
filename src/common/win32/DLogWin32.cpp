7e6558f4e76d78bec419fba9377c5ceca0134dab
sanyuefeiyu <shishuanghe@gmail.com>
2021-08-22 01:32:12 +0800

    modify format

--- src/common/win32/DLogWin32.cpp
+++ src/common/win32/DLogWin32.cpp
@@ -18,9 +18,7 @@ void DLogOutput(DLogMode logMode, DLogLevel level, const char *tag, const char *
     // date time pid tid level tag msg
     int size = _snprintf(outputBuf, LOG_BUF_SIZE + 256, "%04d-%02d-%02d %02d:%02d:%02d:%03d [%d|%d] %s|%s %s\r\n",
         lt.wYear, lt.wMonth, lt.wDay, lt.wHour, lt.wMinute, lt.wSecond, lt.wMilliseconds,
-        GetCurrentProcessId(), GetCurrentThreadId(),
-        gLogLevelDes[level], tag,
-        buf);
+        GetCurrentProcessId(), GetCurrentThreadId(), gLogLevelDes[level], tag, buf);
 
     if (size <= 0) {
         return;
